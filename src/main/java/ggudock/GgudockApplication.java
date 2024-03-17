c5cb98c62e2c0663ad26a27a542930232e32bd7d
kangjjs <emdtod12@naver.com>
2024-03-18 02:06:41 +0900

    feat:초기 개발환경 세팅(#1)

---
+++ src/main/java/ggudock/GgudockApplication.java
@@ -0,0 +1,13 @@
+package ggudock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgudockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgudockApplication.class, args);
+	}
+
+}
