955c34e411faa6be917c5e0506aee1d1befae108
kangjjs <emdtod12@naver.com>
2024-04-14 11:55:16 +0900

    feat:company entity 등록(#3)

---
+++ src/main/java/ggudock/domain/company/entity/CompanyImage.java
@@ -0,0 +1,28 @@
+package ggudock.domain.company.entity;
+
+import ggudock.util.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CompanyImage extends BaseTimeEntity {
+    @Id
+    @Column(name = "company_image_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String picture;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    @Builder
+    public CompanyImage(String picture, Company company) {
+        this.picture = picture;
+        this.company = company;
+    }
+}
