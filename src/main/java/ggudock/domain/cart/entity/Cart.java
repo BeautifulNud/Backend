e51ae032c32f6bdd19d76bfd69b9368fec1fa367
kangjjs <emdtod12@naver.com>
2024-04-14 11:54:00 +0900

    feat:cart entity 등록(#3)

---
+++ src/main/java/ggudock/domain/cart/entity/Cart.java
@@ -0,0 +1,37 @@
+package ggudock.domain.cart.entity;
+
+import ggudock.domain.cart.model.Category;
+import ggudock.domain.item.entity.Item;
+import ggudock.domain.user.entity.User;
+import ggudock.util.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Cart extends BaseTimeEntity {
+    @Id
+    @Column(name = "cart_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    public Cart(Category category, Item item, User user) {
+        this.category = category;
+        this.item = item;
+        this.user = user;
+    }
+}
