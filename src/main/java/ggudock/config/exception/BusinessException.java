8b282f09f880fb7dde4c09da28997995c31a9eb0
qweadzs <qweadzs@naver.com>
2024-04-18 22:01:34 +0900

    feat: BusinessException (#11)
    - 추후 비즈요건에 따라 추가 필요

--- src/main/java/ggudock/config/exception/BusinessException.java
+++ src/main/java/ggudock/config/exception/BusinessException.java
@@ -1,9 +1,26 @@
 package ggudock.config.exception;
 
+import ggudock.config.exception.constant.ErrorCode;
 import lombok.Getter;
 
 @Getter
-public class BusinessException extends RuntimeException{
+public class BusinessException extends RuntimeException {
+    private ErrorCode errorCode;
+    private String message;
+
     public BusinessException() {
+        super();
+        this.errorCode = ErrorCode.UNKNOWN;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public BusinessException(ErrorCode errorCode, String message) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+        this.message = message;
     }
 }
