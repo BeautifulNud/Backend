9fc74e1288b3f1b81252a857f7d60745af6782ac
kangjjs <emdtod12@naver.com>
2024-04-21 15:43:24 +0900

    feat:entity 등록(#3)

---
+++ src/main/generated/ggudock/domain/category/entity/QCategory.java
@@ -0,0 +1,47 @@
+package ggudock.domain.category.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QCategory is a Querydsl query type for Category
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QCategory extends EntityPathBase<Category> {
+
+    private static final long serialVersionUID = 2010783005L;
+
+    public static final QCategory category = new QCategory("category");
+
+    public final ggudock.util.QBaseTimeEntity _super = new ggudock.util.QBaseTimeEntity(this);
+
+    //inherited
+    public final StringPath createdAt = _super.createdAt;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final StringPath modifiedAt = _super.modifiedAt;
+
+    public final StringPath name = createString("name");
+
+    public QCategory(String variable) {
+        super(Category.class, forVariable(variable));
+    }
+
+    public QCategory(Path<? extends Category> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QCategory(PathMetadata metadata) {
+        super(Category.class, metadata);
+    }
+
+}
+
