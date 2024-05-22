b7dd25608b04327ec228e0182ab14317650b5bfd
kangjjs <emdtod12@naver.com>
2024-05-22 22:10:22 +0900

    feat:address errorcode 추가(#24)

--- src/main/java/ggudock/global/exception/constant/ErrorCode.java
+++ src/main/java/ggudock/global/exception/constant/ErrorCode.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     OK(HttpStatus.OK, "정상 처리 되었습니다."),
     NOT_FOUND_ITEM(HttpStatus.NOT_FOUND, "상품을 찾을 수 없습니다."),
+    NOT_FOUND_ADDRESS(HttpStatus.NOT_FOUND, "주소를 찾을 수 없습니다."),
+    NOT_FOUND_DEFAULT_ADDRESS(HttpStatus.NOT_FOUND, "기본 배송지가 설정되어 있지 않습니다."),
     NOT_FOUND_CATEGORY(HttpStatus.NOT_FOUND,"카테고리를 찾을 수 없습니다."),
+    DUPLICATED_ADDRESS(HttpStatus.CREATED,"이미 저장되어 있는 배송지입니다."),
     DUPLICATED_DATE(HttpStatus.CREATED, "이미 상품 요청된 날짜입니다."), // 가정이 요청
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "정상적인 요청이 아닙니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증 토큰이 필요합니다."),
