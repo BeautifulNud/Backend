aa81fd7dc55811656869281238713dcbf592dbdf
hyeonsuk <vkdnjdjxor@naver.com>
2024-05-26 23:28:06 +0900

    refactor:비로그인시 에러 추가(#32)

--- src/main/java/ggudock/config/oauth/utils/SecurityUtil.java
+++ src/main/java/ggudock/config/oauth/utils/SecurityUtil.java
@@ -1,6 +1,8 @@
 package ggudock.config.oauth.utils;
 
 import ggudock.config.oauth.entity.UserPrincipal;
+import ggudock.global.exception.BusinessException;
+import ggudock.global.exception.constant.ErrorCode;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -13,6 +15,10 @@ import java.util.Optional;
 public class SecurityUtil {
     public static String getCurrentName() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication.getPrincipal().equals("anonymousUser"))
+            throw new BusinessException(ErrorCode.DO_NOT_LOGIN);
+
         UserPrincipal oAuth2User = (UserPrincipal) authentication.getPrincipal();
 
         return oAuth2User.getName();
